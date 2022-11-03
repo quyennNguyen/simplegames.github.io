3c29e723f3ec0136160f3d77b06015ddbcc9d9a4
Quyen Nguyen <quyenlearntocode@gmail.com>
2022-11-03 00:48:03 -0400

    updated design

--- superhero/superhero-script.js
+++ superhero/superhero-script.js
@@ -1,48 +1,75 @@
 const getHeroBtn = document.getElementById("get-hero-btn");
 const clearBtn = document.getElementById("clear-btn");
 
-const compCardSet = document.getElementById("comp-hero-set");
-const yourCardSet = document.getElementById("your-hero-set");
+const compHeroSet = document.getElementById("comp-hero-set");
+const yourHeroSet = document.getElementById("your-hero-set");
+
+const numOfCards = 3;
+
+const compSet = [];
+const yourSet = [];
 
 const ACCESS_TOKEN = 2387982781353807;
 const BASE_URL = `https://superheroapi.com/api.php/${ACCESS_TOKEN}`;
 
-const numOfCards = 3;
-
 getHeroBtn.onclick = () => {
-  getHeroSet(numOfCards, compCardSet);
-  getHeroSet(numOfCards, yourCardSet);
+  getCompHeroSet(numOfCards);
+  getYourHeroSet(numOfCards);
 };
 
 clearBtn.onclick = () => {
-  compCardSet.innerHTML = "";
-  yourCardSet.innerHTML = "";
+  compHeroSet.innerHTML = "";
+  yourHeroSet.innerHTML = "";
 };
 
-const getHeroSet = (len, whose) => {
-  if (whose.innerHTML.trim().length == 0) {
+const getCompHeroSet = (len) => {
+  if (compHeroSet.innerHTML.trim().length == 0) {
     for (let i = 0; i < len; i++) {
       let num = Math.floor(Math.random() * 731) + 1;
-      getHero(num, whose);
+      compSet.push(getHero(num, compHeroSet));
+    }
+  }
+};
+
+const getYourHeroSet = (len) => {
+  if (yourHeroSet.innerHTML.trim().length == 0) {
+    for (let i = 0; i < len; i++) {
+      let num = Math.floor(Math.random() * 731) + 1;
+      yourSet.push(getHero(num, yourHeroSet));
     }
   }
 };
 
 const getHero = (id, whose) => {
+  const hero = {};
   fetch(`${BASE_URL}/${id}`)
     .then((result) => result.json())
     .then((json) => {
-      whose.innerHTML += `<div style="border: solid #f4f1de; padding: 5px; display: flex; flex-flow: column; background: #f2cc8f;">
-      <img src="${json.image.url}" alt="superhero" width=300 height=400></img>
-      <br>
-      <p>Name: ${json.name.toUpperCase()}</p>
-      <br>
-      <p>Intelligence: ${json.powerstats.intelligence}</p>
-      <p>Strength: ${json.powerstats.strength}</p>
-      <p>Speed: ${json.powerstats.speed}</p>
-      <p>Durability: ${json.powerstats.durability}</p>
-      <p>Power: ${json.powerstats.power}</p>
-      <p>Combat: ${json.powerstats.combat}</p>
-      </div>`;
+      whose.innerHTML += getHeroInfoStr(json);
+      hero = getHeroStat(json);
     });
+  return hero;
+};
+
+const getHeroInfoStr = (character) => {
+  let name = `<h3>${character.name.toUpperCase()}</h3>`;
+  let image = `<img src="${character.image.url}" alt="superhero" width=225 height=300></img>`;
+  let stats = Object.keys(character.powerstats)
+    .map((stat) => {
+      return `<p>${stat}: ${character.powerstats[stat]}</p>`;
+    })
+    .join("");
+  let infoStr = `<div style="border: solid #f4f1de; padding: 5px; display: flex; flex-flow: column; background: #f2cc8f; cursor: pointer;"> ${name}${image}${stats} </div>`;
+  return infoStr;
+};
+
+const getHeroStat = (character) => {
+  const stats = {};
+  stats.name = character.name;
+  Object.assign(stats, character.powerstats);
+  return stats;
+};
+
+const fight = () => {
+  alert("Noice Hero!");
 };
